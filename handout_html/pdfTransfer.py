9cc538dc2fd9af3a00a677b0a5816e062bb3d46f
BaiiYuan <b05902002@ntu.edu.tw>
2019-02-27 01:22:29 +0800

    jpg and pdf2jpg.py

---
+++ handout_html/pdfTransfer.py
@@ -0,0 +1,17 @@
+import os, sys, tqdm
+from pdf2image import convert_from_path, convert_from_bytes
+# reference: https://github.com/Belval/pdf2image
+
+inputPath = sys.argv[1]
+fileName = inputPath.split("/")[-1].split(".pdf")[0]
+
+print("inputpath: {}, filename: {}".format(inputPath, fileName))
+
+if not os.path.exists("./JPG"):
+	os.mkdir("./JPG")
+if not os.path.exists("./JPG/{}".format(fileName)):
+	os.mkdir("./JPG/{}".format(fileName))
+
+images = convert_from_path(inputPath)
+for i, img in enumerate(images):
+	img.save("./JPG/{}/{}_{}.jpg".format(fileName, fileName, i))
