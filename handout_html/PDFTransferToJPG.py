25c608b71edb08bf45a46e507ce00a84f93c2e61
BaiiYuan <b05902002@ntu.edu.tw>
2019-03-03 15:21:47 +0800

    pdf added

--- handout_html/PDFTransferToJPG.py
+++ handout_html/PDFTransferToJPG.py
@@ -2,6 +2,8 @@ import os, sys, tqdm
 from pdf2image import convert_from_path, convert_from_bytes
 # reference: https://github.com/Belval/pdf2image
 
+# Usage: python PDFTransferToJPG.py ./PDF/W1_Regression.pdf
+
 inputPath = sys.argv[1]
 fileName = inputPath.split("/")[-1].split(".pdf")[0]
 
