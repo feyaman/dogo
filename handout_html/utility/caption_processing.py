7f65c92c038bc1032e8452ad39c47baec179779d
Njceties <Njceties@NjcetiesdeMacBook-Air.local>
2019-03-06 12:12:46 +0800

    Finish all the functionality for youtube.html

--- handout_html/utility/caption_processing.py
+++ handout_html/utility/caption_processing.py
@@ -1,5 +1,12 @@
-with open('../caption/1.html', 'r') as myfile:
-	data=myfile.read().replace('\n', '<br>\n')
-	print(data)
-file = open("../caption/1.html",'w')
-file.write(data)
+with open('../caption/filelist', 'r') as fp:  
+	line = fp.readline()
+	cnt = 0
+	while line:
+		print("Line {}: {}".format(cnt, line.strip()))
+		line = fp.readline()
+		cnt += 1
+		with open('../caption/' + line.strip("\n"), 'r') as myfile:
+			data="<head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'></head>\n" + myfile.read().replace('\n', '<br>\n')
+			print(data)
+		file = open("../caption/" + str(cnt) + ".html",'w')
+		file.write(data)
