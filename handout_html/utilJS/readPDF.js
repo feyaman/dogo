6c4007b2fe74be5c69d9ff8bcba2f11d5a99a418
BaiiYuan <b05902002@ntu.edu.tw>
2019-03-18 14:59:25 +0800

    revise readPDF.js and js related html

--- handout_html/utilJS/readPDF.js
+++ handout_html/utilJS/readPDF.js
@@ -1,9 +1,4 @@
 var Slide_list = [
-	// { pdf_name: 'W1_Regression', pageNumber: 39},
-	// { pdf_name: 'W1_Bias_and_Variance', pageNumber: 22},
-	// { pdf_name: 'W2_Classification', pageNumber: 35},
-	// { pdf_name: 'W2_Gradient_Descent', pageNumber: 39},
-	// { pdf_name: 'W2_Logistic_Regression', pageNumber: 38}
 	{ pdf_name: 'Introduction_of_this_course', pageNumber: 63},
 	{ pdf_name: 'Regression', pageNumber: 39},
 	{ pdf_name: 'Where_does_the_error_come_from?', pageNumber: 22},
@@ -27,7 +22,12 @@ var Slide_list = [
 	{ pdf_name: 'Matrix_Factorization', pageNumber: 8},
 	{ pdf_name: 'Ensemble', pageNumber: 43},
 	{ pdf_name: 'Introduction_of_Structured_Learning', pageNumber: 66},
-	{ pdf_name: 'Introduction_of_Reinforcement_Learning', pageNumber: 57},
+	{ pdf_name: 'Introduction_of_Reinforcement_Learning', pageNumber: 57}
+	// { pdf_name: 'W1_Regression', pageNumber: 39},
+	// { pdf_name: 'W1_Bias_and_Variance', pageNumber: 22},
+	// { pdf_name: 'W2_Classification', pageNumber: 35},
+	// { pdf_name: 'W2_Gradient_Descent', pageNumber: 39},
+	// { pdf_name: 'W2_Logistic_Regression', pageNumber: 38}
 ];
 
 // Initial 
